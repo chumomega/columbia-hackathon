a6e7a26d9900d5e3ab29b601af72b041600e4d6d
chumomega <31514721+chumomega@users.noreply.github.com>
2018-04-15 01:24:09 -0400

    added index

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	return render_template('index.html')
 
 @app.route('/hello')
 def hello():
@@ -22,14 +22,18 @@ def isOdd(num):
 def records():
 	med_record = {'name': 'Chuma', 'age':20, 'blood_type':'O'}
 	return render_template('record.html', record=med_record)
+#html-variable = python-variable
 
 
 @app.route('/people')
 def people():
-	persons = ['chuma', 'phillip', 'chika']
-
+    personsList = ['chuma', 'phillip', 'chika']
+    return render_template('list.html', persons=personsList)
+    
 if __name__ =='__main__':
 	app.run(debug=True, use_reloader=True)
+    
+
 
 
 
